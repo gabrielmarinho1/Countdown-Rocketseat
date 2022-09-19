d25fabf6f377f5f9212bdb520f3b4dd76ea037f9
gabrielmarinho1 <gabriel.marinho112@gmail.com>
2022-09-19 15:02:38 -0300

    modal-finished

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const counter = document.querySelector('.counter')
 const release = document.querySelector('.release')
+const modal = document.querySelector('.modal')
+const open = document.querySelector('.open-modal')
+const close = document.querySelector('.close')
+const submit = document.querySelector('.submit')
 
 function getTimeLeft() {
     var today = new Date();
-    var release_date = new Date(2022 , 11, 20, 22, 40);
+    var release_date = new Date(2022, 11, 20, 22, 40);
     let time = (release_date - today) / 1000;
     let seconds = String(Math.floor(time) % 60).padStart(2, '0')
     let minutes = String(Math.floor(time / 60) % 60).padStart(2, '0')
@@ -15,3 +19,15 @@ function getTimeLeft() {
 }
 
 setInterval(getTimeLeft, 1000)
+
+open.onclick = function () {
+    modal.style.display = "block";
+}
+
+close.onclick = function () {
+    modal.style.display = "none";
+}
+
+submit.onclick = function () {
+    modal.style.display = "none";
+}
